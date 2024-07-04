f49a404bb6c7543dd21fe27d90be1872ce1d7567
Sharma3850 <yogesh.sharma3850@gmail.com>
2024-07-04 19:59:15 +0530

    Add missing files

--- flask - backend/app.py
+++ flask - backend/app.py
@@ -1,29 +1,47 @@
-from flask import Flask, jsonify
-from flask_cors import CORS 
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from random import randint
 
 app = Flask(__name__)
-CORS(app, resources={r"/updates": {"origins": "http://localhost:8100"}})
-# CORS(app) 
-
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/updates', methods=['GET'])
+@app.route('/updates', methods=['GET', 'POST'])
 def updates():
-    data = {
-        'Temperature': randint(0, 50),
-        'Followers': randint(0, 50),
-        'Likes': randint(0, 50),
-        'Stars': randint(0, 50),
-        'Completed': randint(0, 50),
-        'Warnings': randint(0, 50),
-        'Notifications': randint(0, 50)
-    }
+    if request.method == 'GET':
+        data = {
+            'Temperature': randint(0, 50),
+            'Followers': randint(0, 50),
+            'Likes': randint(0, 50),
+            'Stars': randint(0, 50),
+            'Completed': randint(0, 50),
+            'Warnings': randint(0, 50),
+            'Notifications': randint(0, 50)
+        }
+        return jsonify(data)
     
-    return jsonify(data)
+    elif request.method == 'POST':
+        print("hitting post")
+        if request.is_json:
+            data = request.get_json()
+            print(data)
+            return jsonify({"message": "Data received", "data": data})
+        else:
+            return jsonify({"message": "Request must be JSON"}), 400
+        
 
+@app.route('/submit', methods=['POST'])
+def submit_data():
+    print("hitting submit")
+    if request.is_json:
+        data = request.get_json()
+        print(data)
+        return jsonify({"message": "Data received", "data": data})
+    else:
+        return jsonify({"message": "Request must be JSON"}), 400
+        
 if __name__ == '__main__':
     app.run(debug=True)
